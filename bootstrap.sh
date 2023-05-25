76cb36887c0869e4393af884ccee1216cc698ae7
Yury Kachubeyeu <yury_kachubeyeu@greensky.com>
2023-05-25 23:15:50 +0300

    initial bootstra template

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,3 @@
-cdk bootstrap aws://341681825452/eu-central-1 --custom-permissions-boundary eo_role_boundary
+#cdk bootstrap aws://341681825452/eu-central-1 --custom-permissions-boundary eo_role_boundary
+
+cdk bootstrap --show-template > my-bootstrap.yml
