ebcf72ee268ce45305955c0df11f6786eb3856e5
Yury Kachubeyeu <yury_kachubeyeu@greensky.com>
2023-05-26 00:53:47 +0300

    add aspect boundary

--- sample-app/bin/sample-app.ts
+++ sample-app/bin/sample-app.ts
@@ -6,4 +6,29 @@ const app = new cdk.App();
 
 const sampleEnv  = { account: '341681825452', region: 'eu-central-1' };
 
-new SampleAppStack(app, 'SampleAppStack', { env: sampleEnv });
+const sampleStack = new SampleAppStack(app, 'SampleAppStack', { env: sampleEnv });
+
+import {Aspects, IAspect, CfnResource } from 'aws-cdk-lib';
+import { IConstruct } from 'constructs';
+
+export class PermissionsBoundary implements IAspect {
+  private readonly permissionsBoundaryArn: string;
+
+  constructor(permissionBoundaryArn: string) {
+    this.permissionsBoundaryArn = permissionBoundaryArn;
+  }
+
+  public visit(node: IConstruct): void {
+    if (CfnResource.isCfnResource(node) && node.cfnResourceType === 'AWS::IAM::Role') {
+      node.addPropertyOverride('PermissionsBoundary', this.permissionsBoundaryArn);
+    }
+  }
+}
+
+function setPermissionBoundaries(appstack: IConstruct) {
+  Aspects.of(appstack).add(
+    new PermissionsBoundary('arn:aws:iam::341681825452:policy/eo_role_boundary'),
+  );
+}
+
+setPermissionBoundaries(sampleStack)
