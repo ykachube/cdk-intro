c53d5b7c833e3ba55b4a38500651f5a5d57eda59
Yury Kachubeyeu <yury_kachubeyeu@greensky.com>
2023-05-26 00:34:34 +0300

    create sample app

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,4 +3,9 @@
 #https://medium.com/@imageryan/bootstrapping-aws-cdk-in-a-secure-environment-9bc778ea6d94
 #cdk bootstrap --show-template > my-bootstrap.yml
 
- aws cloudformation create-stack --region eu-central-1 --capabilities CAPABILITY_NAMED_IAM --template-body file://my-bootstrap.yml --parameters ParameterKey=PermissionsBoundaryPolicy,ParameterValue=arn:aws:iam::341681825452:policy/eo_role_boundary ParameterKey=CloudFormationExecutionPolicies,ParameterValue=arn:aws:iam::aws:policy/AdministratorAccess --stack-name CDKToolkit
+#aws cloudformation create-stack --region eu-central-1 --capabilities CAPABILITY_NAMED_IAM --template-body file://my-bootstrap.yml --parameters ParameterKey=PermissionsBoundaryPolicy,ParameterValue=arn:aws:iam::341681825452:policy/eo_role_boundary ParameterKey=CloudFormationExecutionPolicies,ParameterValue=arn:aws:iam::aws:policy/AdministratorAccess --stack-name CDKToolkit
+
+mkdir sample-app
+cd sample-app
+cdk init sample-app --language typescript
+
