31934a061d02f564acc192ec1d20762630ec03de
Yury Kachubeyeu <yury_kachubeyeu@greensky.com>
2023-05-26 00:43:53 +0300

    add fargate

--- sample-app/lib/sample-app-stack.ts
+++ sample-app/lib/sample-app-stack.ts
@@ -1,4 +1,4 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
+import { aws_s3, Duration, Stack, StackProps } from 'aws-cdk-lib';
 import * as sns from 'aws-cdk-lib/aws-sns';
 import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
@@ -22,6 +22,25 @@ export class SampleAppStack extends Stack {
     new aws_s3.Bucket(this, 'MyFirstBucket', {
       versioned: true
     });
+
+    const vpc = new aws_ec2.Vpc(this, "MyVpc", {
+      maxAzs: 3 // Default is all AZs in region
+    });
+
+    const cluster = new aws_ecs.Cluster(this, "MyCluster", {
+      vpc: vpc
+    });
+
+    // Create a load-balanced Fargate service and make it public
+    new aws_ecs_patterns.ApplicationLoadBalancedFargateService(this, "MyFargateService", {
+      cluster: cluster, // Required
+      cpu: 512, // Default is 256
+      desiredCount: 6, // Default is 1
+      taskImageOptions: { image: aws_ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample") },
+      memoryLimitMiB: 2048, // Default is 512
+      publicLoadBalancer: true // Default is true
+    });
+
 
   }
 }
